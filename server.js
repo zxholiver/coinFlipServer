e14c1c586236aea1191b2470b92e2dee2eb2f03e
oliver <927639852@qq.com>
2021-08-02 00:40:13 +0800

    登录功能差是否已登录判断，注册功能，连接数据库异常，并且会批量写入数据

--- server.js
+++ server.js
@@ -31,10 +31,10 @@ const pool = mysql.createPool({
 });
 pool.getConnection((err,conn)=>{
     if(err){
-        console.log(`连接失败，err=${err}`);
+        console.log(`数据库连接失败，err=${err}`);
     }
     else{
-        console.log('和mysql数据库连接成功');
+        console.log('mysql数据库连接成功');
         conn.query('select * from userdata', (err2, res) => {
         if (err2) {
             console.log('查询数据库失败');
@@ -43,8 +43,9 @@ pool.getConnection((err,conn)=>{
             for(var i in res){
                 //记录账号密码
                 userList[res[i].id] = {user:res[i].name,pwd:res[i].alexa};
-                console.log(userList);
             }
+            console.log('数据库读取账号密码成功！');
+            console.log(userList);
             conn.destroy();
         }
       });
@@ -66,12 +67,45 @@ var server = ws.createServer((conn)=>{
                     conn.sendText('登录成功！');
                     console.log(`${data.name}已登录！`);
                 }
+                else{
+                    conn.sendText("账号或密码错误！");
+                }
             }   
         }
         //判注册
-        if(data.type == 'register'){
-
-        }
+        // if(data.type == 'register'){
+        //     for(var i in userList){
+        //         //判断是否有账号，有则发送该用户已注册
+        //         if(data.name == userList[i].user){
+        //             conn.sendText('该用户已注册，请勿重复注册！');
+        //             console.log(`${data.name}已注册，请勿重复注册！`);
+        //         }
+        //         else{
+        //             //如果没注册过，则将其写入数据库
+        //             pool.getConnection((err,conn)=>{
+        //                 if(err){
+        //                     console.log(`数据库连接失败，err=${err}`);
+        //                 }
+        //                 else{
+        //                     console.log('mysql数据库连接成功');
+        //                     var sqlLan = 'insert into userdata (name,alexa) values("xiao","1111")';
+        //                     var sqlValue = [data.name,data.pwd];
+        //                     conn.query(sqlLan,(err,res)=>{
+        //                         if(err){
+        //                             console.log('数据库插入失败');
+        //                             ws.sendText("注册失败！");
+        //                         }else{
+        //                             console.log(`${data.name}注册成功！`);
+        //                             conn.sendText("恭喜你，注册成功！");
+        //                             conn.destroy();
+        //                         }
+        //                     });
+        //                 }
+        //             });
+        //         }
+        //     }   
+        // }
+        //----------------------
     });
 }).listen(9511);
 console.log('websocket服务创建成功!');
